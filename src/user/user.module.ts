945c80a2217defc15901e37a0c3c0481fc6498f0
den <denissytnikmacho@gmail.com>
2023-09-07 17:11:58 +0300

    feature: implemented mongoose into user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { AuthService } from './auth.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './user.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
   providers: [UserService, AuthService],
   controllers: [UserController],
 })
