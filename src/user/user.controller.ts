89e45be31995bfc335e003009a82ffb1cf8ef7f3
den <denissytnikmacho@gmail.com>
2023-09-07 19:31:36 +0300

    feature: implemented method for updating user data and added query in getUsers method

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -6,23 +6,25 @@ import {
   Delete,
   Body,
   Param,
+  Query
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { QueryUserDto } from './dtos/query-user.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
 
-  @Get('/')
-  getUsers(@Param('email') email: string): any {
-    return this.userService.find(email);
-  }
-
   @Get('/:id')
   findUser(@Param('id') id: string): any {
     return this.userService.findOne(id);
+  }
+
+  @Get('/')
+  getUsers(@Query() query: QueryUserDto): any {
+    return this.userService.find(query);
   }
 
   @Post('/signup')
@@ -31,7 +33,9 @@ export class UserController {
   }
 
   @Patch('/:id')
-  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto): any {}
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto): any {
+    return this.userService.update(id, body);
+  }
 
   @Delete('/:id')
   removeUser(@Param('id') id: string): any {
