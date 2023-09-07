103796d67d191a9d737265c9668700fd2a8ad11e
den <denissytnikmacho@gmail.com>
2023-09-07 17:10:38 +0300

    feature: implemented mongoose into main module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
+require('dotenv').config();
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, MongooseModule.forRoot(process.env.DATABASE_URI)],
   controllers: [AppController],
   providers: [AppService],
 })
