046e1e937099dd747c58920d1d40f1e8d67fba7c
den <denissytnikmacho@gmail.com>
2023-09-07 17:12:05 +0300

    feature: added user schema

---
+++ src/user/user.schema.ts
@@ -0,0 +1,28 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop({ required: true })
+  username: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ default: 'noavatar' })
+  avatar: string;
+
+  @Prop({ type: mongoose.Schema.Types.Date, default: Date.now })
+  createdAt: Date;
+
+  @Prop({ type: mongoose.Schema.Types.Date, default: Date.now })
+  updatedAt: Date;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
