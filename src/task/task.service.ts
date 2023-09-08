f1ddb072d00964816a819fe67774775d95ab15f2
den <denissytnikmacho@gmail.com>
2023-09-08 18:42:45 +0300

    feature: created and implemented findOne, create, update and remove methods

---
+++ src/task/task.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { NotFoundException } from '@nestjs/common/exceptions';
+
+import { Task } from './task.schema';
+import { CreateTaskDto } from './dto/create-task.dto';
+
+@Injectable()
+export class TaskService {
+  constructor(@InjectModel(Task.name) private taskModel: Model<Task>) {}
+
+  findOne(id: string): Promise<Task> {
+    return this.taskModel.findById(id);
+  }
+
+  find() {}
+
+  async create(createTaskDto: CreateTaskDto): Promise<Task> {
+    return this.taskModel.create({
+      ...createTaskDto,
+      userId: '64fb1939b44653227cee1813',
+    });
+  }
+
+  async update(id: string, attrs: Partial<Task>): Promise<Task> {
+    if (attrs.isCompleted === true) {
+      attrs.dateOfCompletion = new Date();
+    } else if (attrs.isCompleted === false) {
+      attrs.dateOfCompletion = null;
+    }
+
+    const updatedTask = await this.taskModel.findByIdAndUpdate(id, attrs);
+
+    if (!updatedTask) {
+      throw new NotFoundException('Task not found');
+    }
+
+    return updatedTask;
+  }
+
+  remove(id: string): Promise<Task> {
+    return this.taskModel.findByIdAndDelete(id);
+  }
+}
