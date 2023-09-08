de060c4c82e7c5ce709702c079f1ab8de2cfffa7
den <denissytnikmacho@gmail.com>
2023-09-08 16:13:52 +0300

    feature: added password encrypting when registering

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, Query } from 'mongoose';
+import { BadRequestException } from '@nestjs/common/exceptions';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
 
 import { User } from './user.schema';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { QueryUserDto } from './dtos/query-user.dto';
+
+const scrypt = promisify(_scrypt);
 
 @Injectable()
 export class UserService {
@@ -14,12 +19,26 @@ export class UserService {
     return this.userModel.findById(id);
   }
 
-  find(query: QueryUserDto): any {
+  find(query: QueryUserDto): Promise<User[]> {
     return this.userModel.find(query);
   }
 
   async create(createUserDto: CreateUserDto): Promise<User> {
-    const createdUser = new this.userModel(createUserDto);
+    const [foundUser] = await this.find({
+      email: createUserDto.email,
+    } as QueryUserDto);
+    if (foundUser) {
+      throw new BadRequestException('Email already in use');
+    }
+
+    const salt = randomBytes(8).toString('hex');
+    const hash = (await scrypt(createUserDto.password, salt, 32)) as Buffer;
+    const result = salt + '.' + hash.toString('hex');
+
+    const createdUser = new this.userModel({
+      ...createUserDto,
+      password: result,
+    });
     return createdUser.save();
   }
 
@@ -29,7 +48,7 @@ export class UserService {
     return foundUser.save();
   }
 
-  remove(id: string) {
+  remove(id: string): Promise<User> {
     return this.userModel.findByIdAndDelete(id);
   }
 }
