373c0579c289e96778eb7cd8f070cf887f4e8730
den <denissytnikmacho@gmail.com>
2023-09-08 18:41:06 +0300

    feature: created task controller and implemented routes for getting, creating, updating and deleting tasks

---
+++ src/task/task.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Body,
+  Param,
+  Query,
+} from '@nestjs/common';
+
+import { TaskService } from './task.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
+
+@Controller('task')
+export class TaskController {
+  constructor(private taskService: TaskService) {}
+
+  @Get('/:id')
+  getTask(@Param('id') id: string) {
+    return this.taskService.findOne(id);
+  }
+
+  @Get('/')
+  getTasks() {}
+
+  @Post('/')
+  createTask(@Body() body: CreateTaskDto) {
+    return this.taskService.create(body);
+  }
+
+  @Patch('/:id')
+  updateTask(@Param('id') id: string, @Body() body: UpdateTaskDto) {
+    return this.taskService.update(id, body);
+  }
+
+  @Delete('/:id')
+  removeTask(@Param('id') id: string) {
+    return this.taskService.remove(id);
+  }
+}
