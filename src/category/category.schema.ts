d03634c8a50cf8ce07b8efde87dff598ad8f8256
den <denissytnikmacho@gmail.com>
2023-09-10 16:15:47 +0300

    feature: created category schema

---
+++ src/category/category.schema.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
+import { HydratedDocument } from 'mongoose';
+
+import { User } from 'src/user/user.schema';
+
+export type CategoryDocument = HydratedDocument<Category>;
+
+@Schema()
+export class Category {
+  @Prop()
+  title: string;
+
+  @Prop()
+  color: string;
+
+  @Prop({ required: true, type: mongoose.Schema.Types.ObjectId, ref: 'User' })
+  userId: User;
+}
+
+export const CategorySchema = SchemaFactory.createForClass(Category);
