077d30ebf7f0ba405873e2287589c95f647dbc32
den <denissytnikmacho@gmail.com>
2023-09-11 18:07:46 +0300

    style: refactored

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -7,7 +7,9 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './user.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])
+  ],
   providers: [UserService, AuthService],
   controllers: [UserController],
 })
