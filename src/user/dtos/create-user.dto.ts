e1640a8cf3f45039381c324b0daccf4ed68b4323
den <denissytnikmacho@gmail.com>
2023-09-11 13:25:11 +0300

    feature: added limits of length to validation

--- src/user/dtos/create-user.dto.ts
+++ src/user/dtos/create-user.dto.ts
@@ -1,13 +1,16 @@
-import { IsEmail, IsString } from "class-validator";
+import { IsEmail, IsString, Length } from "class-validator";
 
 export class CreateUserDto {
   @IsString()
+  @Length(3, 20)
   username: string;
 
   @IsString()
+  @Length(6, 20)
   password: string;
 
   @IsEmail()
+  @Length(1, 100)
   email: string;
 
   @IsString()
--- src/user/dtos/update-user.dto.ts
+++ src/user/dtos/update-user.dto.ts
@@ -1,12 +1,13 @@
-import { IsEmail, IsString, IsOptional } from 'class-validator';
-import { Transform } from 'class-transformer';
+import { IsEmail, IsString, IsOptional, Length } from 'class-validator';
 
 export class UpdateUserDto {
   @IsString()
+  @Length(3, 20)
   @IsOptional()
   username: string;
 
   @IsEmail()
+  @Length(1, 100)
   @IsOptional()
   email: string;
 
