370ad1ce2b0e2f32d56843e987a8f781d555aad2
den <denissytnikmacho@gmail.com>
2023-09-11 16:55:27 +0300

    feature: implemented method for querying tasks

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -5,6 +5,13 @@ import { NotFoundException } from '@nestjs/common/exceptions';
 
 import { Task } from './task.schema';
 import { CreateTaskDto } from './dtos/create-task.dto';
+import { QueryTaskDto } from './dtos/query-task.dto';
+
+interface queryParams {
+  isCompleted?: boolean;
+  categories?: object;
+  deadline?: object;
+}
 
 @Injectable()
 export class TaskService {
@@ -14,7 +21,54 @@ export class TaskService {
     return this.taskModel.findById(id);
   }
 
-  find() {}
+  find(query: QueryTaskDto): Promise<Task[]> {
+    let queryParams: queryParams = {};
+    const { isCompleted = null, categories = null, deadline = null } = query;
+
+    if (isCompleted) {
+      queryParams.isCompleted = JSON.parse(isCompleted);
+    }
+    if (categories) {
+      queryParams.categories = { $all: JSON.parse(categories) };
+    }
+    if (deadline) {
+      const date = new Date();
+      const year = date.getFullYear();
+      const month =
+        date.getMonth() + 1 < 10
+          ? `0${date.getMonth() + 1}`
+          : date.getMonth() + 1;
+      const day = date.getDate();
+      const todayMidnight = new Date(`${year}-${month}-${day}`);
+
+      if (deadline === 'day') {
+        queryParams.deadline = todayMidnight;
+      } else if (deadline == 'week') {
+        const today = new Date(`${year}-${month}-${day}`);
+        queryParams.deadline = {
+          $gte: todayMidnight,
+          $lte: new Date(today.setDate(today.getDate() + 7)),
+        };
+      } else if (deadline === 'month') {
+        const today = new Date(`${year}-${month}-${day}`);
+        queryParams.deadline = {
+          $gte: todayMidnight,
+          $lte: new Date(today.setMonth(today.getMonth() + 1)),
+        };
+      } else if (deadline === 'year') {
+        queryParams.deadline = {
+          $gte: todayMidnight,
+          $lte: new Date(`${year + 1}-${month}-${day}`),
+        };
+      } else if (deadline === 'outdated') {
+        queryParams.deadline = {
+          $lt: todayMidnight,
+        };
+      }
+    }
+
+    return this.taskModel.find(queryParams).populate('categories');
+  }
 
   async create(createTaskDto: CreateTaskDto): Promise<Task> {
     return this.taskModel.create({
