81009049c728adb1af0ae132ad61e60b42b10430
den <denissytnikmacho@gmail.com>
2023-09-11 18:05:27 +0300

    feature: added jwt module to app imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt/dist';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
@@ -11,6 +13,11 @@ require('dotenv').config();
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.DATABASE_URI),
+    JwtModule.register({
+      global: true,
+      secret: process.env.ACCESS_TOKEN_SECRET,
+      signOptions: { expiresIn: '30d' },
+    }),
     UserModule,
     TaskModule,
     CategoryModule,
