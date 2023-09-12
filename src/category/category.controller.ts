04c5e8592a046ac48e7f7108a01f7062820c4b26
den <denissytnikmacho@gmail.com>
2023-09-12 19:35:19 +0300

    feature: added required authentication to all routes

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -7,39 +7,51 @@ import {
   Body,
   Param,
   Query,
+  Request,
+  Response,
+  UseGuards,
 } from '@nestjs/common';
 
 import { CategoryService } from './category.service';
 import { CreateCategoryDto } from './dtos/create-category.dto';
 import { UpdateCategoryDto } from './dtos/update-category.dto';
 import { QueryCategoryDto } from './dtos/query-category.dto';
+import { AuthGuard } from 'src/guards/auth.guard';
+import { UpdateCategoryPipe } from './pipes/update-category.pipe';
 
 @Controller('category')
+@UseGuards(AuthGuard)
 export class CategoryController {
   constructor(private categoryService: CategoryService) {}
 
   @Get('/:id')
-  getCategory(@Param('id') id: string) {
-    return this.categoryService.findOne(id);
+  getCategory(@Request() req, @Param('id') id: string) {
+    return this.categoryService.findOne(req.user.sub, id);
   }
 
   @Get('/')
-  getCategories(@Query() query: QueryCategoryDto) {
-    return this.categoryService.find(query);
+  getCategories(@Request() req, @Query() query: QueryCategoryDto) {
+    return this.categoryService.find(req.user.sub, query);
   }
 
   @Post('/')
-  createCategory(@Body() body: CreateCategoryDto) {
-    return this.categoryService.create(body);
+  createCategory(@Request() req, @Body() body: CreateCategoryDto) {
+    return this.categoryService.create(req.user.sub, body);
   }
 
   @Patch('/:id')
-  updateCategory(@Param('id') id: string, @Body() body: UpdateCategoryDto) {
-    return this.categoryService.update(id, body);
+  updateCategory(
+    @Request() req,
+    @Param('id') id: string,
+    @Body(UpdateCategoryPipe) body: UpdateCategoryDto,
+  ) {
+    return this.categoryService.update(req.user.sub, id, body);
   }
 
   @Delete('/:id')
-  removeCategory(@Param('id') id: string) {
-    return this.categoryService.remove(id);
+  removeCategory(@Request() req, @Response() res, @Param('id') id: string) {
+    this.categoryService.remove(req.user.sub, id);
+
+    return res.sendStatus(204);
   }
 }
