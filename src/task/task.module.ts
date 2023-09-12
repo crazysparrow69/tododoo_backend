b3ae5113c9db4cbf793684ba2ea48df63ec9e6e2
den <denissytnikmacho@gmail.com>
2023-09-12 19:39:29 +0300

    feature: added user schema to imports

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -4,10 +4,14 @@ import { TaskController } from './task.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { Task, TaskSchema } from './task.schema';
+import { User, UserSchema } from 'src/user/user.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Task.name, schema: TaskSchema }]),
+    MongooseModule.forFeature([
+      { name: Task.name, schema: TaskSchema },
+      { name: User.name, schema: UserSchema },
+    ]),
   ],
   providers: [TaskService],
   controllers: [TaskController],
