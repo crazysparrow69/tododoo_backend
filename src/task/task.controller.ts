e93dc7826a3ad62496ddf216875ce631a75d9ea5
den <denissytnikmacho@gmail.com>
2023-09-12 19:38:01 +0300

    feature: added required authentication to all routes

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -7,39 +7,51 @@ import {
   Body,
   Param,
   Query,
+  UseGuards,
+  Request,
+  Response
 } from '@nestjs/common';
 
 import { TaskService } from './task.service';
 import { CreateTaskDto } from './dtos/create-task.dto';
 import { UpdateTaskDto } from './dtos/update-task.dto';
 import { QueryTaskDto } from './dtos/query-task.dto';
+import { AuthGuard } from 'src/guards/auth.guard';
+import { UpdateTaskPipe } from './pipes/update-task.pipe';
 
 @Controller('task')
+@UseGuards(AuthGuard)
 export class TaskController {
   constructor(private taskService: TaskService) {}
 
   @Get('/:id')
-  getTask(@Param('id') id: string) {
-    return this.taskService.findOne(id);
+  getTask(@Request() req, @Param('id') id: string) {
+    return this.taskService.findOne(req.user.sub, id);
   }
 
   @Get('/')
-  getTasks(@Query() query: QueryTaskDto) {
-    return this.taskService.find(query);
+  getTasks(@Request() req, @Query() query: QueryTaskDto) {
+    return this.taskService.find(req.user.sub, query);
   }
 
   @Post('/')
-  createTask(@Body() body: CreateTaskDto) {
-    return this.taskService.create(body);
+  createTask(@Request() req, @Body() body: CreateTaskDto) {
+    return this.taskService.create(req.user.sub, body);
   }
 
   @Patch('/:id')
-  updateTask(@Param('id') id: string, @Body() body: UpdateTaskDto) {
-    return this.taskService.update(id, body);
+  updateTask(
+    @Request() req,
+    @Param('id') id: string,
+    @Body(UpdateTaskPipe) body: UpdateTaskDto,
+  ) {
+    return this.taskService.update(req.user.sub, id, body);
   }
 
   @Delete('/:id')
-  removeTask(@Param('id') id: string) {
-    return this.taskService.remove(id);
+  removeTask(@Request() req, @Response() res, @Param('id') id: string) {
+    this.taskService.remove(req.user.sub, id);
+    
+    return res.sendStatus(204);
   }
 }
