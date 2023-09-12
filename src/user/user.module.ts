d4cd289732bcbd4077f2d9a243790da75a7c20c7
den <denissytnikmacho@gmail.com>
2023-09-12 19:47:56 +0300

    feature: added task and category schemas to imports

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -5,10 +5,16 @@ import { UserController } from './user.controller';
 import { AuthService } from './auth.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './user.schema';
+import { Task, TaskSchema } from 'src/task/task.schema';
+import { Category, CategorySchema } from 'src/category/category.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      { name: Task.name, schema: TaskSchema },
+      { name: Category.name, schema: CategorySchema },
+    ]),
   ],
   providers: [UserService, AuthService],
   controllers: [UserController],
