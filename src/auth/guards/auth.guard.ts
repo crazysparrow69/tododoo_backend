71a9750164beb0a5fe437bae46c754b973417209
den <denissytnikmacho@gmail.com>
2023-09-13 14:25:01 +0300

    feature: recreated auth guard and added checking for user existance

---
+++ src/auth/guards/auth.guard.ts
@@ -0,0 +1,50 @@
+import { InjectModel } from '@nestjs/mongoose';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import mongoose from 'mongoose';
+import { Model } from 'mongoose';
+
+import { User } from 'src/user/user.schema';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    private jwtService: JwtService,
+    @InjectModel(User.name) private userModel: Model<User>,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: process.env.ACCESS_TOKEN_SECRET,
+      });
+
+      const foundUser = await this.userModel.findById(payload.sub);
+      if (!foundUser) throw new UnauthorizedException();
+
+      payload.sub = new mongoose.Types.ObjectId(payload.sub);
+
+      request['user'] = payload;
+    } catch {
+      throw new UnauthorizedException();
+    }
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
