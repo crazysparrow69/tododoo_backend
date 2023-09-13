9e3af6e80ce1e4af2a4de164c88be430b23497f3
den <denissytnikmacho@gmail.com>
2023-09-13 14:24:36 +0300

    feature: created auth module to use UserModel in AuthGuard

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,13 @@
+import { MongooseModule } from '@nestjs/mongoose';
+import { Module } from '@nestjs/common';
+
+import { AuthGuard } from './guards/auth.guard';
+import { User, UserSchema } from 'src/user/user.schema';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+  providers: [AuthGuard],
+})
+export class AuthModule {}
