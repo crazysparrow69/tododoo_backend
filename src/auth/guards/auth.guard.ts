e7b806ea99c31dc69df222657dbcb416a4bae9b0
den <denissytnikmacho@gmail.com>
2023-09-18 20:20:28 +0300

    style: refactored

--- src/auth/guards/auth.guard.ts
+++ src/auth/guards/auth.guard.ts
@@ -21,9 +21,7 @@ export class AuthGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
-    if (!token) {
-      throw new UnauthorizedException();
-    }
+    if (!token) throw new UnauthorizedException();
 
     try {
       const payload = await this.jwtService.verifyAsync(token, {
