917bd72e43443e41f3fe5a52a2bda167682cae1e
den <denissytnikmacho@gmail.com>
2023-10-07 17:28:16 +0300

    fix: fixed cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ const cors = require('cors');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.use(cors);
+  app.use(cors());
   await app.listen(5000);
 }
 bootstrap();
