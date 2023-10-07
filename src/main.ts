3e71340512bbf42113200d41b536e6eae1da66c7
den <denissytnikmacho@gmail.com>
2023-10-07 17:27:42 +0300

    feature: added cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+const cors = require('cors');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.use(cors);
   await app.listen(5000);
 }
 bootstrap();
