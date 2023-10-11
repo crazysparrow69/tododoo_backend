2aec4c1667e267d9eb2bf92a5f664d1522811252
den <denissytnikmacho@gmail.com>
2023-10-11 16:09:02 +0300

    fix: remade all field validations and added transformations

--- src/task/dtos/query-task.dto.ts
+++ src/task/dtos/query-task.dto.ts
@@ -1,23 +1,34 @@
-import { IsOptional, IsString } from 'class-validator';
+import {
+  IsBoolean,
+  IsOptional,
+  IsString,
+  IsArray,
+  IsNumber,
+} from 'class-validator';
+import { Transform } from 'class-transformer';
 
 export class QueryTaskDto {
-  @IsString()
+  @IsArray()
   @IsOptional()
-  categories: string;
+  @Transform(({ value }) => JSON.parse(value))
+  categories: string[];
 
-  @IsString()
+  @IsBoolean()
   @IsOptional()
-  isCompleted: string;
+  @Transform(({ value }) => JSON.parse(value))
+  isCompleted: boolean;
 
   @IsString()
   @IsOptional()
   deadline: string;
 
-  @IsString()
+  @IsNumber()
   @IsOptional()
+  @Transform(({ value }) => parseInt(value))
   page: number;
 
-  @IsString()
+  @IsNumber()
   @IsOptional()
+  @Transform(({ value }) => parseInt(value))
   limit: number;
 }
