61a87aceb5b8c9eb165b34c9dd21536ad6e6e164
den <denissytnikmacho@gmail.com>
2023-10-12 17:29:47 +0300

    feature: added more invalid params cases to validations

--- src/task/dtos/query-task.dto.ts
+++ src/task/dtos/query-task.dto.ts
@@ -1,25 +1,49 @@
 import {
-  IsBoolean,
   IsOptional,
-  IsString,
   IsArray,
   IsNumber,
 } from 'class-validator';
 import { Transform } from 'class-transformer';
+import { BadRequestException } from '@nestjs/common';
+import { Types } from 'mongoose';
 
 export class QueryTaskDto {
   @IsArray()
   @IsOptional()
-  @Transform(({ value }) => JSON.parse(value))
+  @Transform(({ value }) => {
+    try {
+      const parsed = JSON.parse(value);
+      for (const id of parsed) {
+        if (typeof id !== 'string' || !Types.ObjectId.isValid(id))
+          throw new Error();
+      }
+      return parsed;
+    } catch (err) {
+      throw new BadRequestException('categories must be an array of ObjectId');
+    }
+  })
   categories: string[];
 
-  @IsBoolean()
   @IsOptional()
-  @Transform(({ value }) => JSON.parse(value))
+  @Transform(({ value }) => {
+    try {
+      const parsed = JSON.parse(value);
+      if (typeof parsed !== 'boolean') throw new Error();
+      return parsed;
+    } catch (err) {
+      throw new BadRequestException('isCompleted must be boolean');
+    }
+  })
   isCompleted: boolean;
 
-  @IsString()
   @IsOptional()
+  @Transform(({ value }) => {
+    const deadlines = ['day', 'week', 'month', 'year', 'outdated', 'all'];
+    for (const deadline of deadlines) {
+      if (value === deadline) return value;
+    }
+    throw new BadRequestException('deadline is invalid');
+  })
   deadline: string;
 
   @IsNumber()
