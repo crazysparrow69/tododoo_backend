0189c7b6c3cadcd15f984c415854fd5c81149293
den <denissytnikmacho@gmail.com>
2023-10-25 14:50:10 +0300

    fix: removed Avatar schema from imports

--- src/image/image.module.ts
+++ src/image/image.module.ts
@@ -3,15 +3,11 @@ import { ImageService } from './image.service';
 import { ImageController } from './image.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 
-import { Avatar, AvatarSchema } from './avatar.schema';
 import { User, UserSchema } from 'src/user/user.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([
-      { name: Avatar.name, schema: AvatarSchema },
-      { name: User.name, schema: UserSchema },
-    ]),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   providers: [ImageService],
   controllers: [ImageController],
