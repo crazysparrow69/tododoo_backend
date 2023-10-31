902f5eacc11b84f0cd44327abec4d8767993e71c
den <denissytnikmacho@gmail.com>
2023-10-31 17:21:26 +0200

    test: covered all /user route tests and /category tests

--- test/user.e2e-spec.ts
+++ test/user.e2e-spec.ts
@@ -1,7 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
+import { Types } from 'mongoose';
+
+interface Category {
+  _id: Types.ObjectId;
+  title: string;
+  color: string;
+  userId: Types.ObjectId;
+}
 
 describe('UserController (e2e)', () => {
   let app: INestApplication;
@@ -11,6 +19,7 @@ describe('UserController (e2e)', () => {
     password: '123456',
   };
   let token: string;
+  let category: Category;
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -18,6 +27,9 @@ describe('UserController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({ transform: true, whitelist: true }),
+    );
     await app.init();
   });
 
@@ -31,9 +43,17 @@ describe('UserController (e2e)', () => {
         },
       },
       {
-        message: 'empty username',
+        message: 'too short username',
         data: {
           username: 'а',
+          email: 'sh@gmail.com',
+          password: '123456',
+        },
+      },
+      {
+        message: 'too long username',
+        data: {
+          username: 'clownclownclownclownclown',
           email: 'sh@gmail.com',
           password: '123456',
         },
@@ -50,6 +70,22 @@ describe('UserController (e2e)', () => {
         message: 'empty email',
         data: {
           username: 'test',
+          password: '123456',
+        },
+      },
+      {
+        message: 'invalid email',
+        data: {
+          username: 'test',
+          email: 'clown.com',
+          password: '123456',
+        },
+      },
+      {
+        message: 'invalid type of email',
+        data: {
+          username: 'test',
+          email: true,
           password: '123456',
         },
       },
@@ -155,26 +191,26 @@ describe('UserController (e2e)', () => {
   });
 
   describe('/user (PATCH)', () => {
-    // const datasets = [
-    //   {
-    //     message: 'too short username',
-    //     data: {
-    //       username: 'a',
-    //     },
-    //   },
-    //   {
-    //     message: 'too long username',
-    //     data: {
-    //       username: 'aaaaaaaaaaaaaaaaaaaaahuy',
-    //     },
-    //   },
-    //   {
-    //     message: 'invalid type of username',
-    //     data: {
-    //       username: 123456,
-    //     },
-    //   },
-    // ];
+    const datasets = [
+      {
+        message: 'too short username',
+        data: {
+          username: 'a',
+        },
+      },
+      {
+        message: 'too long username',
+        data: {
+          username: 'aaaaaaaaaaaaaaaaaaaaahuy',
+        },
+      },
+      {
+        message: 'invalid type of username',
+        data: {
+          username: 123456,
+        },
+      },
+    ];
 
     it('should update user data and return updated user', async () => {
       const updatedData = {
@@ -191,16 +227,256 @@ describe('UserController (e2e)', () => {
       expect(response.body.email).toEqual(updatedData.email);
     });
 
-    // datasets.forEach((dataset) => {
-    //   it(`should return an error if request is provided with ${dataset.message}`, async () => {
-    //     const response = await request(app.getHttpServer())
-    //       .patch('/user')
-    //       .send(dataset.data)
-    //       .set('Authorization', `Bearer ${token}`);
+    datasets.forEach((dataset) => {
+      it(`should return an error if request is provided with ${dataset.message}`, async () => {
+        const response = await request(app.getHttpServer())
+          .patch('/user')
+          .send(dataset.data)
+          .set('Authorization', `Bearer ${token}`);
 
-    //     expect(response.statusCode).toBeGreaterThanOrEqual(400);
-    //   }, 10000);
-    // });
+        expect(response.statusCode).toBeGreaterThanOrEqual(400);
+      }, 10000);
+    });
+  });
+
+  describe('/category (POST)', () => {
+    const dataset = [
+      {
+        message: 'empty title',
+        data: {
+          color: 'black',
+        },
+      },
+      {
+        message: 'too short title',
+        data: {
+          title: 'a',
+          color: 'black',
+        },
+      },
+      {
+        message: 'too long title',
+        data: {
+          title: 'sportsportsportsportsport',
+          color: 'black',
+        },
+      },
+      {
+        message: 'invalid type of title',
+        data: {
+          title: 123,
+          color: 'black',
+        },
+      },
+      {
+        message: 'empty color',
+        data: {
+          title: 'sport',
+        },
+      },
+      {
+        message: 'too short color',
+        data: {
+          title: 'sport',
+          color: 'a',
+        },
+      },
+      {
+        message: 'too long color',
+        data: {
+          title: 'sport',
+          color: 'blackblackblackblackblack',
+        },
+      },
+      {
+        message: 'invalid type of color',
+        data: {
+          title: 'sport',
+          color: false,
+        },
+      },
+    ];
+
+    it('should create a category and return it with 201 status code', async () => {
+      const categoryData = {
+        title: 'sport',
+        color: 'gayblue',
+      };
+
+      const response = await request(app.getHttpServer())
+        .post('/category')
+        .send(categoryData)
+        .set('Authorization', `Bearer ${token}`);
+
+      expect(response.statusCode).toBe(201);
+      expect(response.body.title).toEqual(categoryData.title);
+      expect(response.body.color).toEqual(categoryData.color);
+      category = response.body;
+    });
+
+    dataset.forEach((dataset) => {
+      it(`should return an error if request is provided with ${dataset.message}`, async () => {
+        const response = await request(app.getHttpServer())
+          .post('/category')
+          .send(dataset.data)
+          .set('Authorization', `Bearer ${token}`);
+
+        expect(response.statusCode).toBeGreaterThanOrEqual(400);
+      });
+    });
+  });
+
+  describe('/category/:id (GET)', () => {
+    it('should return category with the given id', async () => {
+      const response = await request(app.getHttpServer())
+        .get(`/category/${category._id}`)
+        .set('Authorization', `Bearer ${token}`);
+
+      expect(response.body).toEqual(category);
+    });
+
+    it('should return an error with 400 status code when request url provided with invalid categoryId', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/category/categoryId')
+        .set('Authorization', `Bearer ${token}`);
+
+      expect(response.statusCode).toBe(400);
+    });
+
+    it('should return an error with 404 status code when request url provided with non-existing categoryId', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/category/652461519fd85ce71a666e77')
+        .set('Authorization', `Bearer ${token}`);
+
+      expect(response.statusCode).toBe(404);
+    });
+  });
+
+  describe('/category (GET)', () => {
+    it('should return 401 Bad Request if "page" is not a number', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/category')
+        .query({ page: 'invalid' });
+
+      expect(response.status).toBe(401);
+    });
+
+    it('should return 401 Bad Request if "limit" is not a number', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/category')
+        .query({ limit: 'invalid' });
+
+      expect(response.status).toBe(401);
+    });
+
+    it('should return 200 OK if all query parameters are valid', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/category')
+        .query({ title: 'valid', color: 'valid', page: 1, limit: 10 })
+        .set('Authorization', `Bearer ${token}`);
+
+      expect(response.status).toBe(200);
+    });
+
+    it('should return 200 OK if some query parameters are missing', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/category')
+        .query({ title: 'valid' })
+        .set('Authorization', `Bearer ${token}`);
+
+      expect(response.status).toBe(200);
+    });
+
+    it('should return 400 Bad Request if "color" is not a string', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/category')
+        .query({ color: 123 });
+
+      expect(response.status).toBe(401);
+    });
+  });
+
+  describe('/category/:id (PATCH)', () => {
+    const dataset = [
+      {
+        message: 'too short title',
+        data: {
+          title: 'a',
+          color: 'black',
+        },
+      },
+      {
+        message: 'too long title',
+        data: {
+          title: 'sportsportsportsportsport',
+          color: 'black',
+        },
+      },
+      {
+        message: 'invalid type of title',
+        data: {
+          title: 123,
+          color: 'black',
+        },
+      },
+      {
+        message: 'too short color',
+        data: {
+          title: 'sport',
+          color: 'a',
+        },
+      },
+      {
+        message: 'too long color',
+        data: {
+          title: 'sport',
+          color: 'blackblackblackblackblack',
+        },
+      },
+      {
+        message: 'invalid type of color',
+        data: {
+          title: 'sport',
+          color: false,
+        },
+      },
+    ];
+
+    it('should update category data and return updated category', async () => {
+      const updatedCategory = {
+        title: 'games',
+        color: 'purple',
+      };
+
+      const response = await request(app.getHttpServer())
+        .patch(`/category/${category._id}`)
+        .send(updatedCategory)
+        .set('Authorization', `Bearer ${token}`);
+
+      expect(response.body.title).toEqual(updatedCategory.title);
+      expect(response.body.color).toEqual(updatedCategory.color);
+    });
+
+    dataset.forEach((dataset) => {
+      it(`should return an error if request is provided with ${dataset.message}`, async () => {
+        const response = await request(app.getHttpServer())
+          .patch('/category')
+          .send(dataset.data)
+          .set('Authorization', `Bearer ${token}`);
+
+        expect(response.statusCode).toBeGreaterThanOrEqual(400);
+      });
+    });
+  });
+
+  describe('/category/:id (DELETE)', () => {
+    it('should delete a category and return 204 status code', async () => {
+      const response = await request(app.getHttpServer())
+        .delete(`/category/${category._id}`)
+        .set('Authorization', `Bearer ${token}`);
+
+      expect(response.statusCode).toBe(204);
+    });
   });
 
   describe('/user (DELETE)', () => {
