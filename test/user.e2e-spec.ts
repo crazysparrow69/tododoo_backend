8161249d91b55546c35ee4c343bc24d872cbaa3b
den <denissytnikmacho@gmail.com>
2023-11-15 12:33:22 +0200

    test: commit for running ci/cd

--- test/user.e2e-spec.ts
+++ test/user.e2e-spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
+
 import { AppModule } from '../src/app.module';
 
 describe('User controller (e2e)', () => {
