566d811b624df0f4bedee9aa162c233250c01a50
den <denissytnikmacho@gmail.com>
2023-12-11 20:20:20 +0200

    fix: removed WebsocketGuard

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -3,9 +3,6 @@ import { NotificationGateway } from './notification.gateway';
 
 import { ConfirmationModule } from './../confirmation/confirmation.module';
 import { AuthModule } from 'src/auth/auth.module';
-import { WebsocketGuard } from 'src/auth/guards/websocket.guard';
-import { AuthGuard } from 'src/auth/guards/auth.guard';
-import { JwtModule } from '@nestjs/jwt';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { User, UserSchema } from 'src/user/user.schema';
@@ -16,6 +13,6 @@ import { User, UserSchema } from 'src/user/user.schema';
     ConfirmationModule,
     AuthModule,
   ],
-  providers: [NotificationGateway, WebsocketGuard],
+  providers: [NotificationGateway],
 })
 export class NotificationModule {}
