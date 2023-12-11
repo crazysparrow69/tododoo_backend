0609fb98d423d2eacb4b2b3e1700fb80aa02e319
den <denissytnikmacho@gmail.com>
2023-12-11 20:18:12 +0200

    fix: removed WebsocketGuard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,13 +2,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 
 import { AuthGuard } from './guards/auth.guard';
-import { WebsocketGuard } from './guards/websocket.guard';
 import { User, UserSchema } from '../user/user.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
-  providers: [AuthGuard, WebsocketGuard],
+  providers: [AuthGuard],
 })
 export class AuthModule {}
