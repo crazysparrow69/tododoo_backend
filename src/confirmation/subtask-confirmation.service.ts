2807aeea0853beebd1b7705b887bc141a0ba0f39
den <denissytnikmacho@gmail.com>
2023-12-12 20:09:17 +0200

    feature: created createSubtaskConfirmation and removeSubtaskConfirmation methods

--- src/confirmation/subtask-confirmation.service.ts
+++ src/confirmation/subtask-confirmation.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 
 import { SubtaskConfirmation } from './subtask-confirmation.schema';
 import { CreateSubtaskConfirmationDto } from './dtos/create-subtask-confirmation.dto';
@@ -12,10 +12,30 @@ export class SubtaskConfirmService {
     private subtaskConfirmationModel: Model<SubtaskConfirmation>,
   ) {}
 
-  async createSubtaskConfirmation(userId: string, dto: CreateSubtaskConfirmationDto) {
-    return this.subtaskConfirmationModel.create({
+  async createSubtaskConfirmation(
+    userId: string,
+    dto: CreateSubtaskConfirmationDto,
+  ) {
+    const createdSubtConf = await this.subtaskConfirmationModel.create({
+      _id: new Types.ObjectId(),
       userId,
-      ...dto
+      ...dto,
+    });
+    const populateParams = {
+      path: 'subtaskId',
+      select: 'userId title description deadline',
+    };
+
+    return createdSubtConf.populate(populateParams);
+  }
+
+  removeSubtaskConfirmation(
+    userId: string,
+    subtaskId: string,
+  ): Promise<SubtaskConfirmation> {
+    return this.subtaskConfirmationModel.findOneAndDelete({
+      subtaskId: new Types.ObjectId(subtaskId),
+      userId,
     });
   }
 }
