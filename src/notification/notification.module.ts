d1fa0c2ac76f59158c622a8a8fc13da06532d349
den <denissytnikmacho@gmail.com>
2023-12-14 17:07:19 +0200

    feature: added NotificationService to providers and exports, added NotificationController to controllers, added forwardRef to TaskModule

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { NotificationGateway } from './notification.gateway';
 
 import { ConfirmationModule } from './../confirmation/confirmation.module';
@@ -6,14 +6,19 @@ import { AuthModule } from 'src/auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { User, UserSchema } from 'src/user/user.schema';
+import { NotificationService } from './notification.service';
+import { NotificationController } from './notification.controller';
+import { TaskModule } from 'src/task/task.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     ConfirmationModule,
     AuthModule,
+    forwardRef(() => TaskModule),
   ],
-  providers: [NotificationGateway],
-  exports: [NotificationGateway],
+  providers: [NotificationGateway, NotificationService],
+  exports: [NotificationGateway, NotificationService],
+  controllers: [NotificationController],
 })
 export class NotificationModule {}
