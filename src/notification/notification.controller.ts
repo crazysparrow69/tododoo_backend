db22e1eab684bbd29fea77d456bf1e9bec66eeeb
den <denissytnikmacho@gmail.com>
2023-12-15 15:25:52 +0200

    fix: changed userId type

--- src/notification/notification.controller.ts
+++ src/notification/notification.controller.ts
@@ -3,6 +3,7 @@ import { Controller, Get, UseGuards } from '@nestjs/common';
 import { NotificationService } from 'src/notification/notification.service';
 import { AuthGuard } from 'src/auth/guards/auth.guard';
 import { CurrentUser } from 'src/decorators/current-user.decorator';
+import { Types } from 'mongoose';
 
 @UseGuards(AuthGuard)
 @Controller('notification')
@@ -10,7 +11,7 @@ export class NotificationController {
   constructor(private notificationService: NotificationService) {}
 
   @Get('/')
-  getAllNotifications(@CurrentUser() userId: string) {
+  getAllNotifications(@CurrentUser() userId: Types.ObjectId) {
     return this.notificationService.getAllNotifications(userId);
   }
 }
