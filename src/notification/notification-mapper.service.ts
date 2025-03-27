eeaad4b5223d8ab0e1cc467bdec2f2b8201d338f
crazysparrow69 <denissytnikmacho@gmail.com>
2025-03-27 22:10:19 +0200

    fix: notification mapper

--- src/notification/notification-mapper.service.ts
+++ src/notification/notification-mapper.service.ts
@@ -18,9 +18,12 @@ export class NotificationMapperService {
       return {
         _id: notification._id.toString(),
         userId: notification.userId.toString(),
-        actionByUser: this.userMapperService.toUserReference(
-          notification.actionByUserId
-        ),
+        // TODO: use userMapperService.toUserReference()
+        actionByUser: {
+          _id: notification.actionByUserId._id.toString(),
+          username: notification.actionByUserId.username,
+          avatar: notification.actionByUserId.avatarId?.url ?? "",
+        },
         subtask: {
           _id: notification.subtaskId._id.toString(),
           title: notification.subtaskId.title,
